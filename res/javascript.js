0b7bb7c4c0acc27b7a51fd71fac129d413018eda
koodikirjutaja <vunksandor@gmail.com>
2023-10-08 23:11:57 +0300

    Some comments and minor adjustments

--- res/javascript.js
+++ res/javascript.js
@@ -1,36 +1,43 @@
+// Wait for the DOM to be fully loaded before attaching the event listeners
 document.addEventListener("DOMContentLoaded", function () {
-    // Attach an event listener to the form
+    // Attach an event listener to the login form on its submission
     document.getElementById("loginForm").addEventListener("submit", function (event) {
-        // Prevent the form from submitting (and page from refreshing)
+        // Prevent the default form submission behavior which could lead to a page reload
         event.preventDefault();
 
-        // Call your validation function
+        // Validate the login form; if valid, redirect to the main page
         if (validateLoginForm()) {
-            // Redirect to index.html if the login is valid
             window.location.href = "index.html";
         }
     });
 });
 
+/**
+ * Validate the login form's email and password fields
+ * @returns {boolean} Returns true if the form is valid, false otherwise
+ */
 function validateLoginForm() {
+    // Fetch the values of the email and password fields
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    
+    // Get a reference to the element displaying error messages
     const errorMessageElement = document.getElementById("error-message");
 
-    // Example validation: Check if the fields are not empty (more robust checks can be added as per requirements)
+    // Check if either the email or password fields are empty
     if (email.trim() === "" || password.trim() === "") {
         errorMessageElement.textContent = "Please fill out all the fields.";
         return false;
     }
 
-    // Example validation: Check if the email format is correct (the 'required' attribute on the email input already does a basic check, but this is an additional measure)
+    // Validate the email format using a regular expression pattern
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailRegex.test(email)) {
         errorMessageElement.textContent = "Invalid email format.";
         return false;
     }
 
-    // If all checks pass, return true.
-    errorMessageElement.textContent = "";  // Clear any previous error messages
+    // If all validations pass, clear any error messages and return true to indicate successful validation
+    errorMessageElement.textContent = "";  
     return true;
 }
